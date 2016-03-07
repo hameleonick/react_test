8fea90a7a4490d3070dbf587a17ba54a8243e73e
hameleonick <hameleonick@hameleonicks-MacBook-Pro.local>
2016-03-07 20:27:02 +0200

    options

--- src/js/components/Game/GameLayout.js
+++ src/js/components/Game/GameLayout.js
@@ -2,6 +2,7 @@
  * Created by hameleonick on 2/20/16.
  */
 import React from "react";
+import ModeAction from "../../actions/ModeAction";
 
 export default class GameLayout extends React.Component {
 
@@ -14,12 +15,17 @@ export default class GameLayout extends React.Component {
 
     }
 
+    backToMode() {
+        ModeAction.openOptions("MODE");
+    }
+
     render(){
         if(this.props.currentPage !="GAME")
             return null;
         return (
             <div class="mode-layout">
                 <span>GAME Layout</span>
+                <div onClick={this.backToMode}>Back to MODE</div>
             </div>
         )
     }
